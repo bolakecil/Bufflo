1ae3d6396787280c0bad04f326efcde5f293d303
Jessica L <jeslynn30@gmail.com>
2025-04-07 14:19:58 +0700

    bnrin tampilan sales

--- Bufflo/Sales.swift
+++ Bufflo/Sales.swift
@@ -12,34 +12,34 @@ struct Sales: View {
     let total: Int
     let time: Date
 
-
     var body: some View {
         HStack(alignment: .top) {
             VStack(alignment: .leading) {
                 Text("Rp \(total)")
-                    .font(.system(size: 15, weight: .semibold))
+                    .font(.system(size: 20, weight: .semibold))
+                    .padding(.bottom, 2)
                 Text("\(time, style: .time) | \(time, style: .date)")
-                    .font(.system(size: 13, weight: .regular))
+                    .font(.system(size: 15, weight: .regular))
             }
             .padding(.trailing, 20)
-            let columns =  Array(repeating: GridItem(.flexible(), spacing: 6), count: 3)
+            let columns =  Array(repeating: GridItem(.flexible(), spacing: 8), count: 3)
             Spacer()
             LazyVGrid(columns: columns, alignment: .leading, spacing: 6) {
                 ForEach(items.prefix(6)) { item in
                     HStack(spacing: 4) {
                         Circle()
                             .fill(item.color)
-                            .frame(width: 11, height: 11)
+                            .frame(width: 12, height: 12)
                         Text("x\(item.count)")
-                            .font(.caption)
+                            .font(.system(size: 15, weight: .regular))
                     }
                 }
             }
         }
         .padding(.leading, 25)
-        .padding(.top, 8)
+        .padding(.top, 10)
         Divider()
-            .padding(.top, 4)
+            .padding(.top, 5)
             .padding(.horizontal, 17)
     }
 }
