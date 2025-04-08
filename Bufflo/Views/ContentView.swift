49bbc0b045bcc4265c504e64ab8741ceeb04f57c
Jessica L <jeslynn30@gmail.com>
2025-04-08 15:41:07 +0700

    changed flow of app, removed tabs, made it hierarchical

--- Bufflo/Views/ContentView.swift
+++ Bufflo/Views/ContentView.swift
@@ -1,36 +1,15 @@
 import SwiftUI
 import SwiftData
 
-enum Tab {
-    case customer
-    case sales
-}
-
 struct ContentView: View {
-    @State private var selectedTab: Tab = .customer
-    @State private var salesRecapNeedsVerification = true
-    
+    @State private var needsVerification = true
+
     var body: some View {
-        TabView(selection: $selectedTab){
-            Calculator()
-                .tabItem{
-                    Label("Customer", systemImage: "person.2.fill")
-                }
-                .tag(Tab.customer)
-            needVerify(needVerification: $salesRecapNeedsVerification)
-                .tabItem{
-                    Label("Sales", systemImage: "cart.fill")
-                }
-                .tag(Tab.sales)
-        }
-        .onChange(of: selectedTab) { newTab in
-            if newTab != .sales {
-                salesRecapNeedsVerification = true
-            }
+        NavigationStack {
+            Calculator(needsVerification: $needsVerification)
         }
     }
 }
-
 #Preview {
     ContentView()
 }
